7424e989b0d3026893c1e36aa58fae9bd3c01dc6
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2022-08-31 23:45:37 +0530

    redux-thunk and author

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { postsAdded } from "./postsSlice";
+import { addNewPost } from "./postsSlice";
 import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
@@ -10,6 +10,7 @@ const AddPostForm = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [userId, setUserId] = useState("");
+  const [addRequestStatus, setAddRequestStatus] = useState("idle");
 
   const users = useSelector(selectAllUsers);
 
@@ -17,17 +18,27 @@ const AddPostForm = () => {
   const onContentChanged = (e) => setContent(e.target.value);
   const onAuthorChanged = (e) => setUserId(e.target.value);
 
+  const canSave =
+    [title, content, userId].every(Boolean) && addRequestStatus === "idle";
+
   const onSavePostClicked = () => {
-    if (title && content) {
-      dispatch(postsAdded(title, content, userId));
-      setTitle("");
-      setContent("");
+    if (canSave) {
+      try {
+        setAddRequestStatus("pending");
+        dispatch(addNewPost({ title, body: content, userId })).unwrap();
+
+        setTitle("");
+        setContent("");
+        setUserId("");
+      } catch (err) {
+        console.error("Failed to save the post", err);
+      } finally {
+        setAddRequestStatus("idle");
+      }
     }
   };
 
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
-
-  const userOptions = users.map((user) => (
+  const usersOptions = users.map((user) => (
     <option key={user.id} value={user.id}>
       {user.name}
     </option>
@@ -45,10 +56,10 @@ const AddPostForm = () => {
           value={title}
           onChange={onTitleChanged}
         />
-        <label htmlFor="postAuthot">Author: </label>
+        <label htmlFor="postAuthor">Author:</label>
         <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
           <option value=""></option>
-          {userOptions}
+          {usersOptions}
         </select>
         <label htmlFor="postContent">Content:</label>
         <textarea
@@ -64,5 +75,4 @@ const AddPostForm = () => {
     </section>
   );
 };
-
 export default AddPostForm;
--- src/features/posts/PostAuthor.js
+++ src/features/posts/PostAuthor.js
@@ -5,7 +5,7 @@ const PostAuthor = ({ userId }) => {
   const users = useSelector(selectAllUsers);
 
   const author = users.find((user) => user.id === userId);
-  return <span>by {author ? author.name : "Unknown Author"}</span>;
-};
 
+  return <span>by {author ? author.name : "Unknown author"}</span>;
+};
 export default PostAuthor;
